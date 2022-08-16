cdcc695baaac5574c044c51dad42c10fd33b0ef9
mhassan18015 <hm18015@gmail.com>
2022-08-16 14:22:46 +0200

    update circleci config  file

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/udagram-api-mh.s3-website-us-east-1.amazonaws.com/api/v0',
+  apiHost: 'http://udagram-dev.eba-mznjt2zt.us-east-1.elasticbeanstalk.com/',
 }
 
 /*
